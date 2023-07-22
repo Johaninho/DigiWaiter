2923f0fabd1b05520b82ebec5c88d4969224c5ab
leo <leonelherman22@gmail.com>
2023-07-22 21:21:09 +0300

    Ajout de page Facture et Commande

--- Client/src/app/page.jsx
+++ Client/src/app/page.jsx
@@ -2,11 +2,16 @@
 
 import SingleRestaurant from './pages/SingleRestaurant.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
-
+import { Facture } from './pages/Facture.jsx'
+import FooterSingleRestaurant from '@/components/singleRestaurant/FooterSingleRestaurant.jsx'
+import {Commande} from './pages/Commande.jsx'
 export default function Home() {
   return (
     <ChakraProvider>
-      <SingleRestaurant />
+      {/* <SingleRestaurant /> */}
+      <Facture />
+      {/* <Commande /> */}
+      <FooterSingleRestaurant />
     </ChakraProvider>
   )
 }
