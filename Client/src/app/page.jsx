c70fdd28727f2fcbf9c4f2df0aaee755623c3dca
leo <leonelherman22@gmail.com>
2023-07-23 22:13:02 +0300

    Develop Leo

--- Client/src/app/page.jsx
+++ Client/src/app/page.jsx
@@ -5,11 +5,12 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { Facture } from './pages/Facture.jsx'
 import FooterSingleRestaurant from '@/components/singleRestaurant/FooterSingleRestaurant.jsx'
 import {Commande} from './pages/Commande.jsx'
+
 export default function Home() {
   return (
     <ChakraProvider>
-      <SingleRestaurant />
-      {/* <Facture /> */}
+      {/* <SingleRestaurant /> */}
+      <Facture />
       {/* <Commande /> */}
       <FooterSingleRestaurant />
     </ChakraProvider>
