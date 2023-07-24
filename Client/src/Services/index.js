4d0204fbc9bf6b07e70a49237461c692c901776b
Johaninho Andrianasolo <ninhoandrianasolo@gmail.com>
2023-07-24 21:15:06 +0300

    Index Services

--- Client/src/Services/index.js
+++ Client/src/Services/index.js
@@ -44,3 +44,66 @@ export function getSingleMenu(slug) {
         .catch(console.error)
     }))
 }
+
+export function getCart() {
+    return(new Promise(resolve => {
+        axios.get(`http://localhost:8000/api/cart/`)
+        .then(res => res.status === 200 && res.data)
+        .then(resolve)
+        .catch(console.error)
+    }))
+}
+
+export function addToCart(menuName) {
+    return(new Promise(resolve => {
+        axios.get(`http://localhost:8000/api/cart/add/${menuName}`)
+        .then(res => res.status === 200 && res.data)
+        .then(resolve)
+        .catch(console.error)
+    }))
+}
+
+export function removeToCartCart(menuName) {
+    return(new Promise(resolve => {
+        axios.get(`http://localhost:8000/api/cart/remove/${menuName}`)
+        .then(res => res.status === 200 && res.data)
+        .then(resolve)
+        .catch(console.error)
+    }))
+}
+
+export function getCart() {
+    return(new Promise(resolve => {
+        axios.get(`http://localhost:8000/api/cart/`)
+        .then(res => res.status === 200 && res.data)
+        .then(resolve)
+        .catch(console.error)
+    }))
+}
+
+export function addToCart(menuName) {
+    return(new Promise(resolve => {
+        axios.get(`http://localhost:8000/api/cart/add/${menuName}`)
+        .then(res => res.status === 200 && res.data)
+        .then(resolve)
+        .catch(console.error)
+    }))
+}
+
+export function GetOrder() {
+    return(new Promise(resolve => {
+        axios.get(`http://localhost:8000/api/orders/`)
+        .then(res => res.status === 200 && res.data)
+        .then(resolve)
+        .catch(console.error)
+    }))
+}
+
+export function createOrder(data) {
+    return(new Promise(resolve => {
+        axios.Post(`http://localhost:8000/api/orders/`, data)
+        .then(res => res.status === 200 && res.data)
+        .then(resolve)
+        .catch(console.error)
+    }))
+}
