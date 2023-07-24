cc62d078dff679d601fa91b3847c2c86539c505b
Dilan <dilanlock@outlook.fr>
2023-07-24 15:47:58 +0300

    ajout du router

---
+++ Client/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from '@chakra-ui/react'
+
+const Page = () => {
+    return (
+        <Container>
+            Hello, World! here's the Homepage, and this is a test
+        </Container>
+    )
+}
+
+export default Page
